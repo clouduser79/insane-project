65b53756fc53bfab40277d0c3b508f24a0dbfc06
clouduser79 <swastikmankotia9@gmail.com>
2025-11-02 01:58:43 -0400

    Deploy_GitHub_Pages

--- frontend/app/postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: {
-    // Tailwind plugin removed to avoid version/export mismatches during build.
-    // Keeping autoprefixer for vendor-prefixing.
-    autoprefixer: {},
-  },
-};
